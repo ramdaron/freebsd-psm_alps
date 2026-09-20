286076402d7bb025fef0b7eeb77f3c76115a211c
ramdaron <terehovv@mail.ru>
2026-09-20 09:02:05 +0000

    compat: add typedefs

--- psm.c
+++ psm.c
@@ -107,6 +107,16 @@
 #include <dev/atkbdc/atkbdcreg.h>
 #include <dev/atkbdc/psm.h>
 
+/* Linux speciefic options */
+typedef int8_t   s8;
+typedef int16_t  s16;
+typedef int32_t  s32;
+typedef int64_t  s64;
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
 /* drivers/input/mouse/alps.h */
 #include "alps.h"
 
